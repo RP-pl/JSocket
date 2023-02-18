db0c573601740da80b036d810b3b33ce92ea7caa
RP-pl <68811106+RP-pl@users.noreply.github.com>
2023-02-18 13:34:56 +0100

    Delete Chat directory

--- Chat/src/main/java/Chat/Dao/Message.java
+++
@@ -1,8 +0,0 @@
-package Chat.Dao;
-
-public class Message {
-    public String type;
-    public String author;
-    public String data;
-    public String name;
-}
--- Chat/src/main/java/Chat/Repositories/ChatRepository.java
+++
@@ -1,9 +0,0 @@
-package Chat.Repositories;
-
-import Chat.Dao.Chat;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ChatRepository extends MongoRepository<Chat, Long> {
-    public Chat findChatById(long id);
-
-}
